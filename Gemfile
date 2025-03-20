458bb9701b3a606e9a0f8251ec71883b5341051c
Kiran Rao <hi@kiranrao.ca>
2025-03-20 15:50:50 -0400

    Create setup script

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source 'https://rubygems.org'
 gem 'jekyll'
 gem 'jekyll-sitemap'
+
+gem "webrick", "~> 1.8"
+gem "csv"
+gem "logger"
+gem "base64"
---
+++ setup.sh
@@ -0,0 +1,11 @@
+# Assume MacOS 
+# Assume Homebrew
+
+# Setup base ruby (idenpotent)
+brew install rbenv
+rbenv init
+
+rbenv install 
+rbenv local
+gem install bundler
+bundle install 
